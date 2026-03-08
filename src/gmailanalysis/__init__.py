d422c138a4c72d1243e15bc0ac0895bdffc78962
tsaltd <DEV@AVANTTEK.COM>
2026-03-08 15:44:35 -0400

    UpdateForMigration
    BeforenIntegration

---
+++ src/gmailanalysis/__init__.py
@@ -0,0 +1,13 @@
+C:\Users\wp710\OneDrive\Projects\GmailAnalysis\src\gmailanalysis\__init__.py
+
+sender ID
+EMAIL
+FROM
+SUBJ
+COUNT
+
+
+
+]]
+\python -m pip install --upgrade pip
+python -m pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
